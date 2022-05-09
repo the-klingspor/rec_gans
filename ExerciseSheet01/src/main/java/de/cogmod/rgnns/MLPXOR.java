2c6c9e369298a1f4a8fe211231da0e6800dcdcd1
the-klingspor <j.st@wwu.de>
2022-05-09 22:28:20 +0200

    initial commit, add code for first exercise sheet

---
+++ ExerciseSheet01/src/main/java/de/cogmod/rgnns/MLPXOR.java
@@ -0,0 +1,51 @@
+package de.cogmod.rgnns;
+
+import java.util.Random;
+
+import de.cogmod.rgnns.misc.BasicLearningListener;
+
+/**
+ * @author Sebastian Otte
+ */
+public class MLPXOR {
+    
+    public static void main(String[] args) {
+        //
+        final double[][] input = {
+            {0, 0}, {0, 1}, {1, 0}, {1, 1}
+        };
+        final double[][] target = {
+            {0}, {1}, {1}, {0}
+        };
+        //
+        final Random rnd = new Random(100L);
+        //
+        // set up network. biases are used by default, but
+        // be deactivated using net.setBias(layer, false),
+        // where layer gives the layer index (1 = the first hidden layer).
+        // 
+        final MultiLayerPerceptron net = new MultiLayerPerceptron(2, 20, 1);
+        //
+        // perform training.
+        //
+        final int epochs = 10000;         // don't change this value!
+        final double learningrate = 0.01;
+        final double momentumrate = 0.5;
+        //
+        // generate initial weights.
+        //
+        net.initializeWeights(rnd, 0.1);
+        //
+        net.trainStochastic(
+            rnd, 
+            input,
+            target,
+            epochs,
+            learningrate,
+            momentumrate,
+            new BasicLearningListener()
+        );
+        //
+    }
+
+}
---
+++ ExerciseSheet01/src/main/java/de/cogmod/rgnns/misc/BasicLearningListener.java
@@ -0,0 +1,11 @@
+package de.cogmod.rgnns.misc;
+
+/**
+ * @author Sebastian Otte
+ */
+public class BasicLearningListener implements LearningListener {
+    @Override
+    public void afterEpoch(final int epoch, final double trainingerror) {
+        System.out.println("epoch: " + epoch + " training error: " + trainingerror);
+    }
+}
